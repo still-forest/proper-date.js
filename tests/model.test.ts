323c791b1ee147d2b7f961a48f90b0bfe20efb54
John Szymanowski <github@john.szymanow.ski>
2025-03-13 20:09:38 +0800

    Fix test

--- tests/model.test.ts
+++ tests/model.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, test } from "vitest";
-import { ProperDate } from "../lib/model";
+import ProperDate from "../lib/model";
 
 describe("ProperDate", () => {
   describe("constructor", () => {
