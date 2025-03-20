a6868b044147110aa183cf4f1347ea599ef69b9e
John Szymanowski <github@john.szymanow.ski>
2025-03-20 14:18:14 +0800

    Don't use toUTCDateTime

--- lib/interface.ts
+++ lib/interface.ts
@@ -1,26 +1,26 @@
-import type { Period } from "./types";
+import type { Period } from "./types"
 
 export interface ProperDateInterface {
-  year: number;
-  month: number;
-  day: number;
-  formatted: string;
-  priorYearEnd: ProperDateInterface;
-  priorMonthEnd: ProperDateInterface;
-  equals(other: ProperDateInterface): boolean;
-  formatDateDifference(other: ProperDateInterface): string;
-  getTime(): number;
-  toString(): string;
-  toJSON(): string;
-  toDate(): Date;
-  toUTCDatetime(): Date;
-  getTime(): number;
-  add(n: number, period: Period): ProperDateInterface;
-  subtract(n: number, period: Period): ProperDateInterface;
-  addDays(days: number): ProperDateInterface;
-  getDateNDaysAgo(n: number): ProperDateInterface;
-  getDateNMonthsAgo(n: number): ProperDateInterface;
-  getDateNYearsAgo(n: number): ProperDateInterface;
-  getEndOfNMonthsAgo(n: number): ProperDateInterface;
-  getEndOfNYearsAgo(n: number): ProperDateInterface;
+  year: number
+  month: number
+  day: number
+  formatted: string
+  priorYearEnd: ProperDateInterface
+  priorMonthEnd: ProperDateInterface
+  equals(other: ProperDateInterface): boolean
+  formatDateDifference(other: ProperDateInterface): string
+  getTime(): number
+  toString(): string
+  toJSON(): string
+  toDate(): Date
+  toUTCDatetime(): Date // deprecated
+  getTime(): number
+  add(n: number, period: Period): ProperDateInterface
+  subtract(n: number, period: Period): ProperDateInterface
+  addDays(days: number): ProperDateInterface // deprecated
+  getDateNDaysAgo(n: number): ProperDateInterface // deprecated
+  getDateNMonthsAgo(n: number): ProperDateInterface // deprecated
+  getDateNYearsAgo(n: number): ProperDateInterface // deprecated
+  getEndOfNMonthsAgo(n: number): ProperDateInterface // deprecated
+  getEndOfNYearsAgo(n: number): ProperDateInterface // deprecated
 }
