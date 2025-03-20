b50daba31ed3c125cbda79163b45cb9f7df3290d
John Szymanowski <github@john.szymanow.ski>
2025-03-20 21:53:18 +0800

    Tweak biome settings

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -70,9 +70,7 @@ describe("utils", () => {
       const expectedErrorMessage =
         "Date must be either a Date, ProperDate, or YYYY-MM-DD formatted string";
 
-      expect(() => parseInput("my birthday last year")).toThrowError(
-        expectedErrorMessage
-      );
+      expect(() => parseInput("my birthday last year")).toThrowError(expectedErrorMessage);
       // @ts-expect-error Testing invalid input
       expect(() => parseInput(123)).toThrow(expectedErrorMessage);
       // @ts-expect-error Testing invalid input
