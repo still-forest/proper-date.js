9c4f6a886fd22bef4840dd087d506f8db5c0a892
John Szymanowski <github@john.szymanow.ski>
2026-01-14 07:19:16 +0800

    Ensure Today respects the client timezone

--- lib/model.ts
+++ lib/model.ts
@@ -4,7 +4,9 @@ import type { ArithmeticOptions, Period } from "./types";
 import { buildLocalDate, buildUTCDate, parseInput } from "./utils";
 
 const getDefaultDate = (): string => {
-  return new Date().toISOString().split("T")[0];
+  const d = new Date();
+  const dateStrInClientTimeZone = `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(2, "0")}-${String(d.getDate()).padStart(2, "0")}`;
+  return dateStrInClientTimeZone;
 };
 
 export default class ProperDate {
